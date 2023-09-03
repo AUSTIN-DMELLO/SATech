b7e2e271d4d15f2d69311be08c08db7f4d701402
Austin Dmello <austindmello2004@outlook.com>
2023-09-03 20:23:58 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/technology.jpg";
+import image from "../images/circuit.jpg";
 
-const imageAltText = "technology picture";
+const imageAltText = "circuit picture";
 
 /**
  * Sort description that expands on your title on the Home component.
